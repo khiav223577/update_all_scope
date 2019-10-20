0893f1305ff9684c4ac5a34d486174799bac2ed9
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2019-10-21 00:39:07 +0800

    add test cases

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,42 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "update_all_scope"
+# frozen_string_literal: true
 
-require "minitest/autorun"
+require 'simplecov'
+SimpleCov.start
+
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'update_all_scope'
+
+require 'minitest/autorun'
+
+case ENV['DB']
+when 'mysql'
+  require 'lib/mysql2_connection'
+when 'pg'
+  require 'lib/postgresql_connection'
+else
+  fail 'please run test cases by: `rake test DB=mysql` or `rake test DB=pg`'
+end
+
+require 'lib/patches'
+require 'lib/seeds'
+
+def in_sandbox
+  ActiveRecord::Base.transaction do
+    yield
+    fail ActiveRecord::Rollback
+  end
+end
+
+def assert_queries(expected_count, event_key = 'sql.active_record')
+  sqls = []
+  subscriber = ActiveSupport::Notifications.subscribe(event_key) do |_, _, _, _, payload|
+    sqls << "  ● #{payload[:sql]}" if payload[:sql] !~ /\A(?:BEGIN TRANSACTION|COMMIT TRANSACTION|BEGIN|COMMIT)\z/i
+  end
+  yield
+  if expected_count != sqls.size # show all sql queries if query count doesn't equal to expected count.
+    assert_equal "expect #{expected_count} queries, but have #{sqls.size}", "\n#{sqls.join("\n").gsub('"', "'")}\n"
+  end
+  assert_equal expected_count, sqls.size
+ensure
+  ActiveSupport::Notifications.unsubscribe(subscriber)
+end
--- test/update_all_scope_test.rb
+++ test/update_all_scope_test.rb
@@ -1,11 +1,12 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class UpdateAllScopeTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::UpdateAllScope::VERSION
+  def setup
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_that_it_has_a_version_number
+    refute_nil ::UpdateAllScope::VERSION
   end
 end
