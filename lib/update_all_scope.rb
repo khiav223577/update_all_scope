fed42cdba2439564fde4ca230e91f364f43a70bf
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2019-10-21 00:21:32 +0800

    initialize gem

---
+++ lib/update_all_scope.rb
@@ -0,0 +1,6 @@
+require "update_all_scope/version"
+
+module UpdateAllScope
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require "update_all_scope"
+
+require "minitest/autorun"
