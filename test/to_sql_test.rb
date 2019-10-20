0e50b378a68c94f2a891b96eb691077bfb566bad
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2019-10-21 01:01:51 +0800

    test to_sql

--- test/to_sql_test.rb
+++ test/to_sql_test.rb
@@ -9,4 +9,15 @@ class UpdateAllScopeTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::UpdateAllScope::VERSION
   end
+
+  def test_update_name_with_where_condition
+  	scope = UpdateAllScope::UpdateAllScope.new(relation: User.where(id: -1))
+  	scope.update(name: 'wolf')
+
+    assert_equal_in_dbs(
+      scope.to_sql, 
+      pg: %{UPDATE "users" SET "name" = 'wolf' WHERE "users"."id" = -1},
+      mysql: %{UPDATE `users` SET `users`.`name` = 'wolf' WHERE `users`.`id` = -1}
+    )
+  end
 end
